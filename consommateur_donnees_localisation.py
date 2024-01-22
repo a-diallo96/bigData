1e51257b327955db655c029ec2e5d18ac45254e5
aissatou <ashitadiallo1996@gmail.com>
2024-01-22 11:08:08 +0100

    test du json

--- consommateur_donnees_localisation.py
+++ consommateur_donnees_localisation.py
@@ -5,7 +5,7 @@ from kafka import KafkaConsumer
 consommateur = KafkaConsumer('donnees_localisation', group_id='group_donnees_localisation', bootstrap_servers='localhost:9092', enable_auto_commit=False,  auto_offset_reset='earliest')
 
 for msg in consommateur:
-    # Afficher la clé et les données lues
+    ligne = json.loads(msg.value)
     print(f'Topic: {msg.topic}, Key: {msg.key.decode()}, Value: {msg.value}')
     consommateur.commit()
 
--- consommateur_type_route.py
+++ consommateur_type_route.py
@@ -5,7 +5,7 @@ from kafka import KafkaConsumer
 consommateur = KafkaConsumer('type_route', group_id='group_route', bootstrap_servers='localhost:9092', enable_auto_commit=False,  auto_offset_reset='earliest')
 
 for msg in consommateur:
-    # Afficher la clé et les données lues
+    ligne = json.loads(msg.value)
     print(f'Topic: {msg.topic}, Key: {msg.key.decode()}, Value: {msg.value}')
     consommateur.commit()
 
